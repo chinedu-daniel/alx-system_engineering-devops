3afb63f364f15f953bbf363996a16124a85696d4
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-05-03 20:17:23 +0200

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -4,5 +4,5 @@ exec { 'kill_killmenow_process':
   command     => '/usr/bin/pkill killmenow',
   path        => '/usr/bin',
   onlyif      => '/usr/bin/pgrep killmenow',
-  refreshonly => true,
+  refreshonly => true\n,
 }
