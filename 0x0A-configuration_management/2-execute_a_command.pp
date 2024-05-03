8db8ea4be95017962f0be4c2e8ec81ee67420544
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-05-03 20:14:47 +0200

    2-execute_a_command.pp

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Puppet manifest to kill a process named "killmenow"
+
+exec { 'kill_killmenow_process':
+  command     => '/usr/bin/pkill killmenow',
+  path        => '/usr/bin',
+  onlyif      => '/usr/bin/pgrep killmenow',
+  refreshonly => true,
+}
