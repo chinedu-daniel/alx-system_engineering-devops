8ef1a31cc552a15a7197dfb6c968a37500c401fb
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-05-04 01:51:58 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Puppet manifest to kill a process named "killmenow"
 
 exec { 'pkill_killmenow':
-    command => '/usr/bin/pkill killmenow'
-    path    => '/usr/bin:/usr/sbin:/bin\n',
+    path    => '/usr/bin:/usr/sbin:/bin',
 }
