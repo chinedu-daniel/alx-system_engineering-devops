c3c41c1e97969ad070431e430ec093c0d69cf7e5
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-06-19 19:18:31 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -23,7 +23,10 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            if 'data' in data and 'subscribers' in data['data']:
+                return data['data']['subscribers']
+            else:
+                return 0
         else:
             return 0
     except requests.RequestException:
