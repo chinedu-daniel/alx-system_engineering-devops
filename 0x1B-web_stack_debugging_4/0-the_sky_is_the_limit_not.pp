94df11ca11b2b47fcc2cc5b2f71e73b8a5804ae9
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-06-28 14:05:16 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,18 +2,18 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command	=> 'sudo sed -i "s/15/4096/" /etc/default/nginx',
-  path		=> ['/bin', '/usr/bin'],
-  logoutput	=> true,
-  refreshonly	=> true,
+  command     => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
+  path        => ['/bin', '/usr/bin'],
+  logoutput   => true,
+  refreshonly => true,
 }
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command	=> 'sudo systemct1 restart nginx.service',
-  path		=> ['/bin', '/usr/bin'],
-  logoutput	=> true,
-  refreshonly	=> true,
-  subscribe	=> Exec['fix--for-nginx'],
+  command     => 'sudo systemctl restart nginx.service',
+  path        => ['/bin', '/usr/bin'],
+  logoutput   => true,
+  refreshonly => true,
+  subscribe   => Exec['fix--for-nginx'],
 }
 
